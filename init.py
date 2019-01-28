d1f97fb2c9f4780718cacb27022ab6d502607887
valentinbus <valentinoiho@gmail.com>
2019-01-28 19:21:17 +0100

    feature avec init

--- init.py
+++ init.py
@@ -1,47 +1,53 @@
 import mysql.connector
 from classes import Sql, CollectData
+from constants import CATEGORY
 
-cnx = mysql.connector.connect(
-    user = 'root',
-    password = 'root',
-    host = 'localhost',
-    database = 'Openfoodfact'
-)
 
-cursor = cnx.cursor()
+def init_db():
 
-try:
-    cursor.execute('drop table substitue;')
-    cnx.commit()
-    cursor.close()
-
+    sql = Sql('root', 'root', 'localhost', 'Openfoodfact') #tous les paramêtres pour établir la connexion
+    cnx = sql.connect_db()
+    
     cursor = cnx.cursor()
-    cursor.execute('drop table product;')
-    cnx.commit()
-    cursor.close()
 
-    cursor = cnx.cursor()
-    cursor.execute('drop table category;')
-    cnx.commit()
-    cursor.close()
-except mysql.connector.errors.ProgrammingError as err:
-    print('La base de donnée est déjà clean')
+    try:
+        cursor.execute('drop table substitue;')
+        cnx.commit()
+        cursor.close()
 
+        cursor = cnx.cursor()
+        cursor.execute('drop table product;')
+        cnx.commit()
+        cursor.close()
 
-
-sql = Sql('root', 'root', 'localhost', 'Openfoodfact') #tous les paramêtres pour établir la connexion
-sql.create_database() #initialise les tables de la db
-
-
-###MAP DATA###
-
-for i in range(len(CATEGORY)):
-    cat_number = sql.map_category(CATEGORY[i]) #insert la catégorie et retourne l'id de la catégorie
-
-    init_collect_data = CollectData('https://fr.openfoodfacts.org/categorie/') #initialise le lien générique qui sera modifié en fonction des catégories 
+        cursor = cnx.cursor()
+        cursor.execute('drop table category;')
+        cnx.commit()
+        cursor.close()
+    except mysql.connector.errors.ProgrammingError as err:
+        print('La base de donnée est déjà clean')
 
 
-    url = init_collect_data.create_url(CATEGORY[i]) #crée l'url de la catégorie en question
-    init_collect_data.json_to_dict(cat_number) #crée le dict qui sera insérer dans la table product et insére la foreign key a savoir l'id de la catégorie de la table category
-    query = init_collect_data.dict_to_insert_query() #crée la query pour insérer les datas 
-    sql.map_data(query)
+    sql.create_database() #initialise les tables de la db
+
+
+    ###MAP DATA###
+
+    for i in range(len(CATEGORY)):
+        cat_number = sql.map_category(CATEGORY[i]) #insert la catégorie et retourne l'id de la catégorie
+
+        init_collect_data = CollectData('https://fr.openfoodfacts.org/categorie/') #initialise le lien générique qui sera modifié en fonction des catégories 
+
+
+        init_collect_data.create_url(CATEGORY[i]) #crée l'url de la catégorie en question
+        init_collect_data.json_to_dict(cat_number) #crée le dict qui sera insérer dans la table product et insére la foreign key a savoir l'id de la catégorie de la table category
+        query = init_collect_data.dict_to_insert_query() #crée la query pour insérer les datas 
+        sql.map_data(query)
+
+
+print('JE SUIS UN CHAMPION !!!!')
+
+if __name__ == "__main__":
+    print('JE SUIS dans le main !!!!')
+    init_db()
+
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-import requests
-import json
-import pprint
+import argparse
+from init import init_db
 
 
-pp = pprint.PrettyPrinter(indent=4)
 
-with open('./local/categories.json', 'r') as file:
-    data = file.read()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', '-i', action = 'store_true', help='initalise the db')
+    parser.add_argument('--drop', '-d')
+    args = parser.parse_args()
+    if args.init:
+        init.init_db()
+
+
+if __name__ == "__main__":
+    main()
